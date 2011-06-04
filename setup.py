29c836fe8d3ba9d9ae8aaf42d893f9e7836061bb
fish2000 <fish2000@gmail.com>
2011-06-04 01:39:38 -0400

    Moved LICENSE.txt and added stub setup.py file.

---
+++ setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(
+    name='nodebox-color',
+    version='1.9.4.6-f2k',
+    description='Color classes for python, from the Nodebox project v1.0-series source.',
+    author='Alexander Bohn',
+    author_email='fish2000@gmail.com',
+    url='http://github.com/fish2000/nodebox-color',
+    packages=[
+        'colors',
+    ],
+    package_data={
+        'colors': [
+            'aggregated/adjectives/*.xml',
+            'aggregated/basic_english/*.xml',
+            'aggregated/emotion/*.xml',
+            'aggregated/nature/*.xml',
+            'context/*.txt',
+        ],
+    },
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Nodebox',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Public License v2',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Utilities'
+    ],
+)
